624b5779926d5936a3385cc45a0d685a3f08632d
Prashant Singh <prashantsh7014@gmail.com>
2025-04-15 00:56:22 +0530

    update mail path

--- server/utils/emailService.js
+++ server/utils/emailService.js
@@ -6,7 +6,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.GMAIL_USER,
-    pass: process.env.GMAIL_PASS
+    pass: process.env.MAIL_PASS
   }
 });
 
